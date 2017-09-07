ab798f83ccea7a8956c3f4b2a4f5adaee5f6dc9a
esfsd <esfsd@user78>
2017-09-07 15:27:12 +0900

    신청자 매칭목록 나열 완료 (상태에 따라 필터링만 하면됨)

--- src/com/bepum/web/controller/bepumi/SearchingMatchingListController.java
+++ src/com/bepum/web/controller/bepumi/SearchingMatchingListController.java
@@ -15,14 +15,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.bepum.web.dao.BepumiMatchingDao;
+import com.bepum.web.dao.SearchingMatchingDao;
+import com.bepum.web.dao.jdbc.JdbcBepumiMatchingDao;
+import com.bepum.web.dao.jdbc.JdbcSearchingMatchingDao;
+
 @WebServlet("/searching/matching-list")
 public class SearchingMatchingListController extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/*response.sendRedirect("notice.jsp");*/
-	
+
+		String id = "yeonjoo";
+		String _query = request.getParameter("search");  //얘 "매칭완료" 아니고 "search"인 이유가 궁금하다
 		
-		request.getRequestDispatcher("/WEBINF/views/searching/matching/list.jsp").forward(request, response);
+		String _page = request.getParameter("p");
+
+		int page = 1;
+		if (_page != null && !(_page.equals("")))
+			page = Integer.parseInt(_page);
+		
+		String query = "";
+		if (_query != null && !(_query.equals("")))
+			query = _query;
+		
+		
+		SearchingMatchingDao dao = new JdbcSearchingMatchingDao();
+		request.setAttribute("list", dao.getList(id, page, query));
+		request.setAttribute("count", dao.getCount(id));
+		
+		request.getRequestDispatcher("/WEB-INF/views/searching/matching/list.jsp").forward(request, response);
 
 	}
 }
---
+++ src/com/bepum/web/dao/SearchingMatchingDao.java
@@ -0,0 +1,16 @@
+package com.bepum.web.dao;
+
+import java.util.List;
+
+import com.bepum.web.entity.MatchingView;
+
+public interface SearchingMatchingDao {
+
+	List<MatchingView> getList(String id, int page, String query);
+	
+	List<MatchingView> get(String id, String no);
+
+	
+	int getCount(String id);
+
+}
