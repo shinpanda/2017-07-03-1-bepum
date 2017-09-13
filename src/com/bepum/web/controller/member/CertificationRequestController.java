0e82ece3507f9b82ae045d809491517f5a3caeb4
user <user@user96>
2017-09-13 09:05:16 +0900

    자격심사 약관동의 완성

--- src/com/bepum/web/controller/member/CertificationRequestController.java
+++ src/com/bepum/web/controller/member/CertificationRequestController.java
@@ -1,6 +1,7 @@
 package com.bepum.web.controller.member;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,17 +9,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.bepum.web.dao.BepumiRequestDao;
+import com.bepum.web.dao.jdbc.JdbcBepumiRequestDao;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
 @WebServlet("/member/certification-request")
 public class CertificationRequestController extends HttpServlet {
 
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String uri  = request.getRequestURI();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+/*		String uri  = request.getRequestURI();
 		
-		request.setAttribute("uri", uri);
+		request.setAttribute("uri", uri);*/
 		
 		/*response.sendRedirect("notice.jsp");*/
 		request.getRequestDispatcher("/WEB-INF/views/member/certification/request.jsp").forward(request, response);
-
 	}
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.sendRedirect("certification-identify");
+	}
+	
 }
